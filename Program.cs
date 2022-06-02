ae38594e0b9c82950a12a900f9e60c7ce15f918c
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-02 19:10:12 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetPizza
+{
+
+
+    public class Pizza
+    {
+        public string nom { get; set; }
+        public float prix { get; set; }
+        public bool vegetarienne = false;
+
+        public Pizza()
+        {
+         
+        }
+
+        public Pizza(string nom, float prix,bool vegetarienne)
+        {
+            this.nom = nom;
+            this.prix = prix;
+            this.vegetarienne = vegetarienne;
+        }
+
+        public void afficher()
+        {
+            if (vegetarienne)
+            {
+                Console.WriteLine($" nom : {nom} (V) - prix : {prix} € ");
+            }
+            else
+            {
+                Console.WriteLine($" nom : {nom} - prix : {prix} € ");
+            }
+           
+
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            var listPizza = new List<Pizza>() { 
+
+                new Pizza() { nom = "4 fromages", prix = 10.5f, vegetarienne = true },
+                 new Pizza() { nom = "Napolitaine", prix = 13.5f, vegetarienne = false },
+                  new Pizza() { nom = "4 saisons", prix = 12f, vegetarienne = true },
+                   new Pizza() { nom = "LA vegetarienne", prix = 11.5f, vegetarienne = true },
+            };
+
+
+            foreach (var pizza in listPizza)
+            {
+                pizza.afficher();
+            }
+        }
+    }
+}
