18bf49a5c19b2785340ae7036702c07bd95e3c76
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-02 19:32:28 +0200

    mise en majuscule des noms de pizza

--- Program.cs
+++ Program.cs
@@ -28,11 +28,21 @@ namespace ProjetPizza
 
           
             string badgeVegetarienne = vegetarienne ?  " (V) " : "";
+            string nomMinuscules = nom.ToLower();
+            string nomMajuscule = nom.ToUpper();
+            // cette synthaxe permet de modifier le nom sans alterer la variable nom instance de la classe Pizza
+            //  nomMinuscules[1..] cette synthaxe permet de dire que toutes les autres caracteres seront en minuscules
+            string nomAfficher = nomMajuscule[0] + nomMinuscules[1..];
+            // on aurait pu faire egalement 
+            //string nomAfficher1 = nomMajuscule[0] + nomMinuscules.Substring(1);
 
-            Console.WriteLine($" nom : {nom} {badgeVegetarienne} - prix : {prix} € ");
+            Console.WriteLine($" nom : {nomAfficher} {badgeVegetarienne} - prix : {prix} € ");
           
                 
         }
+
+     
+        
 
     }
 
@@ -45,11 +55,11 @@ namespace ProjetPizza
             var listPizza = new List<Pizza>() { 
 
                 new Pizza() { nom = "4 fromages", prix = 10.5f, vegetarienne = true },
-                new Pizza() { nom = "Napolitaine", prix = 13.5f, vegetarienne = false },
+                new Pizza() { nom = "napolitaine", prix = 13.5f, vegetarienne = false },
                 new Pizza() { nom = "4 saisons", prix = 12f, vegetarienne = true },
-                new Pizza() { nom = "LA vegetarienne", prix = 11.5f, vegetarienne = true },
-                new Pizza() { nom = "Indienne", prix = 14.5f, vegetarienne = false },
-                new Pizza() { nom = "Calzone", prix = 9.5f, vegetarienne = false },
+                new Pizza() { nom = "vegetarienne", prix = 11.5f, vegetarienne = true },
+                new Pizza() { nom = "indienne", prix = 14.5f, vegetarienne = false },
+                new Pizza() { nom = "calzone", prix = 9.5f, vegetarienne = false },
             };
 
 
