844791666898f4667e67a531419d12c1fefcd24f
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-04 17:12:49 +0200

    Communication Reseau

--- Program.cs
+++ Program.cs
@@ -10,17 +10,7 @@ namespace ProjetPizza
 
 
 
-    class Personne
-    {
-        public string Name { get; set; }
-        public int age { get; set; }
 
-
-        public void afficher()
-        {
-            Console.WriteLine($" {Name} -  {age} ans");
-        }
-    }
 
 
 
@@ -31,41 +21,7 @@ namespace ProjetPizza
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 
-           List<Personne> listpersonne = new List<Personne>() 
-            { 
-                new Personne() { Name="Alber",age=30},
-                new Personne() { Name="Richard",age=23},
-                new Personne() { Name="Camille",age=45},
-                new Personne() { Name="Delphine",age=32 }
-            };
-
-            var json = JsonConvert.SerializeObject(listpersonne);
-            var infoPersonne = new StringBuilder();
-            infoPersonne.Append(json.ToString());
-            var path = "out";
-            var filename = "infoPersonne.txt";
-            var pathAndFile = Path.Combine(path, filename);
-
-            Console.WriteLine("Sauvegarde des données executées");
-           File.WriteAllText(pathAndFile, infoPersonne.ToString());
-            Console.WriteLine("Sauvegarde des données Terminées");
-            Console.WriteLine(json);
-
-
-
-            Console.WriteLine(" lecture des données");
-            using(var stream = File.OpenText(pathAndFile))
-            {
-                while (true)
-                {
-                    var line = stream.ReadLine();
-                    if (line == null) { break; }
-
-                    List<Personne> person = JsonConvert.DeserializeObject<List<Personne>>(line);
-                    person.ForEach(personne => personne.afficher());
-                }
-            }
-
         }
+
     }
 }
