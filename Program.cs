baf623b597f7d1498ff804b91e6113b372f8c92c
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-04 16:22:20 +0200

    nettoyage du code

--- Program.cs
+++ Program.cs
@@ -31,43 +31,7 @@ namespace ProjetPizza
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 
-            //var personne = new Personne();
-            //personne.Name = "Billy";
-            //personne.age = 45;
-
-            //personne.afficher();
-
-
-            ///* information la serialisation et deserialisation
-            // se base sur l'accessibilité aux variable
-            //pour cela il est preferable de definir des getters et setters
-            //afin que celles ci soient deserialisé car le principe de deserialisation
-            //se base sur l'accessibilité des variables si celles si sont private , ce ne sera pas possible
-            //Ou prevoir un constructeur dans la classe qui rappel ces memes variables
-
-            //la serialisation elle se base sur get de la variable 
-            //*/
-
-            ////serialisation de l'objet Personne
-            //var json = JsonConvert.SerializeObject(personne);
-            //Console.WriteLine(json);
-
-          
-
-            //Console.WriteLine("Sauvegarde des données executées");
-            //File.WriteAllText(pathAndFile, infoPersonne.ToString());
-            //Console.WriteLine("Sauvegarde des données Terminées");
-
-
-            ////deserialisation d'un fichier json 
-            //var jsonlist = File.ReadAllLines(pathAndFile);
-
-            //foreach (var item in jsonlist)
-            //{
-            //    Personne person = JsonConvert.DeserializeObject<Personne>(item);
-            //    person.afficher();
-            //}
-            List<Personne> listpersonne = new List<Personne>() 
+           List<Personne> listpersonne = new List<Personne>() 
             { 
                 new Personne() { Name="Alber",age=30},
                 new Personne() { Name="Richard",age=23},
