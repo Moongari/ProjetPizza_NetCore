e1fcf31a53d0c5020003f45838518bff345bf485
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-04 15:56:44 +0200

    commentaires

--- Program.cs
+++ Program.cs
@@ -37,6 +37,16 @@ namespace ProjetPizza
             personne.afficher();
 
 
+            /* information la serialisation et deserialisation
+             se base sur l'accessibilité aux variable
+            pour cela il est preferable de definir des getters et setters
+            afin que celles ci soient deserialisé car le principe de deserialisation
+            se base sur l'accessibilité des variables si celles si sont private , ce ne sera pas possible
+            Ou prevoir un constructeur dans la classe qui rappel ces memes variables
+
+            la serialisation elle se base sur get de la variable 
+            */
+
             //serialisation de l'objet Personne
             var json = JsonConvert.SerializeObject(personne);
             Console.WriteLine(json);
