af9b73040015354c9d076e3b58d20871fed07a88
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-05 19:40:30 +0200

    global using aspect

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿
 
+global using Newtonsoft.Json; // utilisation d'un global using
+// permet de le rendre disponible sur l'ensemble des classes du projet.
+using ProgrammeDateTime;
+
 Console.WriteLine("Hello C#10 .NET 6");
 
-var l = new List<int>();
+var p = new Personne() { name = "Toto", infoPer = 4 };
+
+var json = JsonConvert.SerializeObject(p);
+Console.WriteLine(json);
