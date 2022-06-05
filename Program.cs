3261245f5558bdeda2dd15f2c23689b119d3b295
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-05 13:34:08 +0200

    mode asynchrone via delegate

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading;
 
 namespace ProgrammeDateTime
 {
@@ -15,18 +18,46 @@ namespace ProgrammeDateTime
 
     public class Program
     {
-        
+        static bool isDownload = false;
       
         static void Main(string[] args)
         {
-            
 
+            //Exemple lors du telechargement d'une image importante
+            // 1) version synchrone 
+            var url = "https://codeavecjonathan.com/res/bateau.jpg";
+            
+            var webClient = new WebClient(); 
+
+            //Console.Write("Telechargement en cours...");
+            //webClient.DownloadFile(url, "bateau.jpg");
+            //Console.Write(" Terminé ");
+
+            // 2) version Asynchrones 
+
+            Console.Write("Telechargement en cours...");
+         
+            webClient.DownloadFileCompleted += DownloadFileCompleted;
+            webClient.DownloadFileAsync(new Uri(url), "bateau.jpg");
 
            
+            while (!isDownload)
+            {
+                Thread.Sleep(500);
+                Console.Write(".");
+                
+            }
+
+
+            Console.Write(" Fin du programme ");
 
         }
 
-
-
+        private static void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
+            Console.WriteLine("Téléchargement terminé");
+            isDownload = true;
+        }
     }
 }
