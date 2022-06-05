6a3f4ef438f45368866121deabbe1d8eb69ac57d
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-05 15:39:28 +0200

    nouveaute c#9

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace ProgrammeDateTime
 
 
 
-    // Fonction asynchrone : Async /await
+    // Nouveaute C#9
+    // Top-Level-Programs
 
     
 
@@ -22,60 +23,17 @@ namespace ProgrammeDateTime
     {
        
       
-        static async Task Main(string[] args)
-        {
-            string url1 = "https://codeavecjonathan.com/res/dummy.txt";
-            string url2 = "https://codeavecjonathan.com/res/pizzas1.json";
-
-            Console.WriteLine("Téléchargement.....");
-            var displayTask = infoTelechargement();
-            var task1 = downLoadData(url1);
-            var task2 = downLoadData(url2);
-
-            //await task1;
-            //await task2;
-
-            // on peut egalement ecrire cela ainsi
-            await Task.WhenAll(task1,task2);
-            Console.WriteLine("Fin du programme");
-        }
-
-
-
-
-        //Task correspond a une methode void il est preferable d'utiliser Task que void
-        // si l'on desire renvoye une valeur on utilisera Task<String> avec un return dans la methode.
-        static async Task downLoadData(string url)
+        static void  Main(string[] args)
         {
             
-
-            try
-            {
-                var httpClient = new HttpClient();
-                var resultat = await httpClient.GetStringAsync(url);
-            }
-            catch (HttpRequestException ex)
-            {
-                var statusCode = ex.StatusCode;
-                //Console.WriteLine("probleme de telechargement" + ex);
-                Console.WriteLine("Erreur " + statusCode);
-            }
-            
-
-            //Console.WriteLine($"{resultat}");
-            Console.WriteLine("ok =>"+url);
         }
 
 
-        static async Task infoTelechargement()
-        {
-            string info = ".";
-            while (true)
-            {
-                await Task.Delay(500);
-                Console.Write(info);
-            }
-        }
+
+
+       
+
+       
         
     }
 }
