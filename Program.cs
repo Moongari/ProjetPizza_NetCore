85398dafdf67e79a83a481fb9512d63390e1342c
Moustafa moungari <moungari.moustafa@gmail.com>
2022-06-05 13:55:57 +0200

    Fonction async await .net

--- Program.cs
+++ Program.cs
@@ -12,56 +12,19 @@ namespace ProgrammeDateTime
 
 
 
-    // Fonction asynchrone : Delegate ou CallBack
+    // Fonction asynchrone : Async /await
 
     
 
     public class Program
     {
-        static bool isDownload = false;
+       
       
         static void Main(string[] args)
         {
 
-            //Exemple lors du telechargement d'une image importante
-            // 1) version synchrone 
-            var url = "https://codeavecjonathan.com/res/bateau.jpg";
-            
-            var webClient = new WebClient(); 
-
-            //Console.Write("Telechargement en cours...");
-            //webClient.DownloadFile(url, "bateau.jpg");
-            //Console.Write(" Terminé ");
-
-            // 2) version Asynchrones 
-
-            Console.Write("Telechargement en cours...");
-         
-            webClient.DownloadFileCompleted += DownloadFileCompleted;
-            webClient.DownloadFileAsync(new Uri(url), "bateau.jpg");
-
-           
-            while (!isDownload)
-            {
-                Thread.Sleep(500);
-                if (!isDownload)
-                {
-                    Console.Write(".");
-                }
-                
-                
-            }
-
-
-            Console.Write(" Fin du programme ");
-
         }
 
-        private static void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-        {
-            
-            Console.WriteLine("Téléchargement terminé");
-            isDownload = true;
-        }
+        
     }
 }
